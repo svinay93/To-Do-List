72e50138a8d4bba7bbf381d650eb28459e4ddd98
svinay93 <31494589+svinay93@users.noreply.github.com>
2019-06-03 03:55:10 -0500

    Final Commit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-mongoose.connect("mongodb+srv://admin-sunil:Mccon1993@cluster0-lhxxz.mongodb.net/todolistDB",{useNewUrlParser:true});
+mongoose.connect("mongodb+srv://admin-sunil:*******@cluster0-lhxxz.mongodb.net/todolistDB",{useNewUrlParser:true});
 
 const itemSchema = {
   name : String
